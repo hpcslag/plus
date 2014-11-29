e97592c726e8268d3491cb786436a198138409a1
unknown <cslag@hotmail.com.tw>
2014-11-29 12:21:21 +0800

    initi

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 //This index.js test Framework Server (Test)
-
-require('./lib/server')({},80,'127.0.0.1',function(){
-	console.log("Server Run as 127.0.0.1");
-});
+appone.host(conf.host.ip).map('/',path.join(__dirname,'./www'))
+	.get('/hello',function(req,res,cb) {
+		cb(false,'/hello',{
+			type: 'text/html',
+			data: '<html><body>hello world</body></html>'
+		});
+	}).get('/index',function(req,res,cb){
+		var data = view.render('index.ejs',{
+			name:"Test Name"
+		},function(callback){
+			cb(false,'/index',{
+				type:'text/html',
+				data: callback
+			});
+		});
+	});
